cad17b3deb683dc3e2ff6650ea129e573973270e
Deepak K V <deepudeepz383@gmail.com>
2025-01-17 18:46:24 +0530

    Existing user email check done for login

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,8 +1,19 @@
+import { db } from "@/lib/dbConfig";
 import { NextResponse } from "next/server";
 
 export async function POST(req:Request){
     try{
         const body = await req.json();
+        const{email, username, password} = body;
+
+
+        //Check existing emails:-
+        const existingUserByEmail = await db.user.findUnique({
+            where: {email: email}
+        });
+        if(existingUserByEmail){
+            return NextResponse.json({user:null, message:"User email already exists!"},{status:409})
+        }
 
         return NextResponse.json(body)
     }catch(error){
